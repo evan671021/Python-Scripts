e7c31f083e9af9f1aff93408e1811492e10a7e07
evan671021 <63328718+evan671021@users.noreply.github.com>
2020-04-12 16:16:40 -0400

    Update number_guess.py
    adding try: and except: with ValueError

--- Number Guess/number_guess.py
+++ Number Guess/number_guess.py
@@ -5,17 +5,18 @@ def number_guess(num):
     num = randint(1, 1000)
     print('I\'m going to think of a number between 1 and 1000')
     while True:
-        guess = input('\nTry to guess the number. ')
-        guess = int(guess)
-        if guess > num:
-            print('Your guess is too high.')
-        elif guess < num:
-            print('Your guess is too low.')
-        elif guess == num:
-            print('Good Job! You guessed the number correctly!')
-            time.sleep(5)
-            break
-        else:
-            print('Please type a number.')
+        try:
+            guess = input('\nTry to guess the number. ')
+            guess = int(guess)
+            if guess > num:
+                print('Your guess is too high.')
+            elif guess < num:
+                print('Your guess is too low.')
+            elif guess == num:
+                print('Good Job! You guessed the number correctly!')
+                time.sleep(5)
+                break
+        except ValueError:
+            print('\nERROR: Numbers only\n')
 
 number_guess(num)
