f8fcbd9707500d53cb6a7f621c426e8c398014a1
evan671021 <63328718+evan671021@users.noreply.github.com>
2020-04-12 14:49:27 -0400

    Create number_guess.py

---
+++ Number Guess/number_guess.py
@@ -0,0 +1,19 @@
+from random import randint
+num = None
+def number_guess(num):
+    num = randint(1, 1000)
+    print('I\'m going to think of a number between 1 and 1000')
+    while True:
+        guess = input('\nTry to guess the number. ')
+        guess = int(guess)
+        if guess > num:
+            print('Your guess is too high.')
+        elif guess < num:
+            print('Your guess is too low.')
+        elif guess == num:
+            print('Good Job! You guessed the number correctly!')
+            break
+        else:
+            print('Please type a number.')
+
+number_guess(num)
