34d8fa07838114000aa7c588daabe22b22d58ca3
evan671021 <63328718+evan671021@users.noreply.github.com>
2020-04-13 15:23:02 -0400

    Update pig_latin_translator.py

--- Pig Latin Translator/pig_latin_translator.py
+++ Pig Latin Translator/pig_latin_translator.py
@@ -2,8 +2,8 @@ vowels = ['a','e','i','o','u']
 word = input('Enter a word.')
 first = word[0]
 if first in vowels:
-    letters = []
-    letters.append(list(word))
-    letters.append('a''y')
+    answer = word + 'ay'
+else:
+    answer = word[1:] + first + 'ay'
     
-print(letters)
+print(answer)
