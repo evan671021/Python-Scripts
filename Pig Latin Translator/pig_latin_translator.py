0a83fa52621ebe9b6f4b19e0167d5188fd841120
evan671021 <63328718+evan671021@users.noreply.github.com>
2020-04-13 15:26:06 -0400

    Update pig_latin_translator.py

--- Pig Latin Translator/pig_latin_translator.py
+++ Pig Latin Translator/pig_latin_translator.py
@@ -1,9 +1,12 @@
-vowels = ['a','e','i','o','u']
-word = input('Enter a word.')
-first = word[0]
-if first in vowels:
-    answer = word + 'ay'
-else:
-    answer = word[1:] + first + 'ay'
-    
-print(answer)
+while True:
+    vowels = ['a','e','i','o','u']
+    word = input('Enter a word.')
+    first = word[0]
+    if first in vowels:
+        answer = word + 'ay'
+    elif: word == 'quit':
+        break
+    else:
+        answer = word[1:] + first + 'ay'
+
+    print(answer)
